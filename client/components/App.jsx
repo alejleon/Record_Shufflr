34e8270a53dd7d4ca3f7be766645ce0d506df5ce
alejleon <aherna4@gmail.com>
2021-07-07 13:02:26 -0500

    repo setup complete

---
+++ client/components/App.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+
+      <header className="App-header">
+        Helloooo
+        <p>
+          From App.js
+        </p>
+
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
